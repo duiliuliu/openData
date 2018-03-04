815d1f9139ed2b398e37c671e3b174f52e6a375d
pengruizxy <pengrui55555@163.com>
2018-03-04 09:52:46 +0800

    对佛山目录数据的获取进行优化、格式规范化

--- openData/fsSpider.py
+++ openData/fsSpider.py
@@ -1,19 +1,47 @@
 # *-* encoding = utf-8 *-* 
 # author: pengr
 
+'''佛山基础数据资源目录'''
+
 from spider import Spider,Writer
-import time
 
 def func(response = None,  data=None, header = None):
     if response:
         return response.json()['data']
 
     if data:
-        tmp = []
-        for i in data[0]['update_time']:
-            if i:
-                tmp.append(i)
-        data[0]['update_time'] = tmp
+        for item in data:
+            #获取到的更新时间有空值与重复值
+            item['update_time'] = mytrim(item['update_time'])
+
+            #去除简介空值
+            item['description'] = mytrim(item['description'])
+            
+            #获取到的更新频率与数据格式为编号，转化编号
+            if item['conf_update_cycle'] == '4':
+                item['conf_update_cycle'] = '每月'
+            elif item['conf_update_cycle'] == '7':
+                item['conf_update_cycle'] = '每年'
+            else:
+                item['conf_update_cycle'] = '不详'
+                
+            format_list = item['conf_use_type'].strip().split(',')
+            format_dict = {
+                '1':'数据集',
+                '2':'文件集',
+                '3':'API服务',
+                '4':'地图',
+                '':''
+            }
+            tmp = []
+            for l in format_list:
+                tmp.append(format_dict[l])
+            item['conf_use_type'] = ' '.join(tmp)
+
+            #所属主题与所属行业所标识的键为同一键
+            item['topic_name'] = item['group_name'][1]
+            item['group_name'] = item['group_name'][0]
+                
 
     if header:
         myheader = {
@@ -22,10 +50,12 @@ def func(response = None,  data=None, header = None):
             'file_count':'文件数',
             'api_count':'接口数量',
             'open_type':'开放状态',
-            'group_name':'经济建设',
+            'topic_name':'所属主题',
             'update_time':'最后更新',
             'org_name':'来源部门',
             'cata_tags':'标签',
+            'conf_update_cycle':'更新频率',
+            'conf_use_type':'数据格式',
             'released_time':'发布时间',
             'group_name':'所属行业',
             'description':'简介',
@@ -36,22 +66,22 @@ def func(response = None,  data=None, header = None):
             'use_points':'评分总数',
             'use_scores':'平均评分',
             'header_sort':[
-                'cata_title','data_count','file_count','api_count','open_type','group_name','update_time','org_name',
-                'cata_tags','released_time','group_name','description','use_file_count','use_visit','use_grade','use_task_count',
-                'use_points','use_scores'
+                'cata_title','data_count','file_count','api_count','open_type','topic_name','update_time','org_name','cata_tags',
+                'conf_update_cycle','conf_use_type','released_time','group_name','description','use_file_count','use_visit','use_grade',
+                'use_task_count','use_points','use_scores'
             ]
         }
 
+        header['topic_name'] = ''
         header['myheader'] = myheader
-        
-def gettime(timestamp):
-    try:
-        timestamp = timestamp /1000
-        time_local = time.localtime(timestamp)
-        return time.strftime("%Y-%m-%d %H:%M:%S",time_local)
-    except :
-        return timestamp
-    
+ 
+#去除空值
+def mytrim(item):
+    tmp = []
+    for i in item:
+        if i:
+            tmp.append(i)
+    return ' '.join(tmp)   
 
 if __name__ == '__main__':
     ''' 佛山数据  66页'''
